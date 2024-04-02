65f8c7e221c708ed3518abb36ee2c7302e35ba22
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2024-04-02 11:54:47 +0200

    InputSystem

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerJump : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+
+    private BoxCollider2D _boxCollider2D;
+
+    private PlayerInput _playerInput;
+
+    private bool _isJumping;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _playerInput = GetComponent<PlayerInput>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnJump()
+    {
+        if (!_isJumping)
+        {
+            _isJumping = true;
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, 10f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Sol")
+        {
+            _isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Sol")
+        {
+            _isJumping = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerInput _input;
+
+    private float _speed = 5f;
+
+    private void Start()
+    {
+        _input = GetComponent<PlayerInput>();
+    }
+
+    // Méthode pour que le joueur puisse bouger
+    private void OnMove()
+    {
+        Vector2 direction = _input.actions.FindAction("Move").ReadValue<Vector2>();
+        transform.position += new Vector3(direction.x, 0, 0) * Time.deltaTime * _speed;
+    }
+
+    private void FixedUpdate()
+    {
+        // La méthode OnMove() est mis dans un FixedUpdate pour que les movements du joueurs soient fluides
+        OnMove();
+    }
+}
