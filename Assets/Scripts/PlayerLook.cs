3a8b8a6991639bf6b3f8e872c997e17d447f01bc
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2024-04-03 12:36:30 +0200

    Mouse Point

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerInput _playerInput;
+
+    private float _sensibility = 5f;
+
+    private void Start()
+    {
+        StartCoroutine(MousePoint());
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnLook()
+    {
+        Vector2 direction = _playerInput.actions.FindAction("Look").ReadValue<Vector2>();
+        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * _sensibility;
+    }
+
+    private void FixedUpdate()
+    {
+        OnLook();
+    }
+
+    private IEnumerator MousePoint()
+    {
+        yield return new WaitForSeconds(1);
+        transform.position = new Vector3(0, 0, 0);
+    }
+}
