26dbfd3cbd02cbf4910ba1578264e5761ca29d21
Bryan Berge <berge.bryan@gmail.com>
2024-04-03 16:01:57 +0200

    effect

---
+++ Assets/Scripts/ScreenShake.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour
+{
+    public float shakeDuration = 0.1f;
+    public float shakeAmount = 0.5f; 
+
+    private Vector3 originalPosition;
+
+    void Start()
+    {
+        originalPosition = transform.localPosition;
+    }
+
+    public void Shake()
+    {
+        if (shakeDuration > 0)
+        {
+            transform.localPosition = originalPosition + Random.insideUnitSphere * shakeAmount;
+            shakeDuration -= Time.deltaTime;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            transform.localPosition = originalPosition;
+        }
+    }
+}
