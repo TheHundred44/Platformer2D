5e7f9924c664ab3a6d81deb7b826ff84e5013082
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2024-04-03 09:45:00 +0200

    HP

---
+++ Assets/Scripts/EnemyHp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class EnemyHp : MonoBehaviour
+{
+    public int _hp = 500;
+
+    [SerializeField]
+    private Color _damageColor;
+
+    public virtual void LowerHealth(int damage)
+    {
+        if (_hp > 0)
+        {
+            _hp -= damage;
+        }
+
+        if (_hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    //other.gameObject.SendMessage("LowerHealth", damage, SendMessageOptions.DontRequireReceiver);
+}
---
+++ Assets/Scripts/PlayrHP.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayrHP : MonoBehaviour
+{
+    private int score = 0;
+
+    private int hp = 3;
+
+    [SerializeField] private Image SpriteHealBar0;
+    [SerializeField] private Image SpriteHealBar1;
+    [SerializeField] private Image SpriteHealBar2;
+    [SerializeField] private Image SpriteHealBar3;
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            hp--;
+            OnLosingHealth();
+        }
+
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.tag == "Heal")
+        {
+            if (hp < 3)
+            {
+                hp++;
+                Destroy(other.gameObject);
+                OnRegainingHealth();
+            }
+            else
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+
+    private void OnLosingHealth()
+    {
+        switch (hp)
+        {
+            case 2:
+                SpriteHealBar3.gameObject.SetActive(false);
+                break;
+            case 1:
+                SpriteHealBar2.gameObject.SetActive(false);
+                break;
+            case 0:
+                SpriteHealBar1.gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    private void OnRegainingHealth()
+    {
+        switch (hp)
+        {
+            case 3:
+                SpriteHealBar3.gameObject.SetActive(true);
+                break;
+            case 2:
+                SpriteHealBar2.gameObject.SetActive(true);
+                break;
+        }
+    }
+}
