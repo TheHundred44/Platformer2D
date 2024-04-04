20a189159153137d0650b900f03534c4fa5ea6a4
Bryan Berge <berge.bryan@gmail.com>
2024-04-04 11:11:42 +0200

    win condition

---
+++ Assets/Scripts/PlayerButtons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PlayerButtons : MonoBehaviour
+{
+    private PlayerInput _playerInput;
+
+    [SerializeField] private GameObject _canvaOptions;
+
+    private void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+    }
+
+    public void OnRetry()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+
+    public void OnOptions()
+    {
+        if(_canvaOptions.activeSelf)
+        {
+            _canvaOptions.SetActive(true);
+        }
+        else
+        {
+            _canvaOptions.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    public GameObject CanvaWin;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Time.timeScale = 0f;
+            CanvaWin.SetActive(true);
+        }
+    }
+}
